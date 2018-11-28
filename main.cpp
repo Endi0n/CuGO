aabf428c63c98cfaadd2ac1f9628b73c7a8dcc46
Zumza123 <coctavian10@yahoo.ro>
2018-11-28 19:11:37 +0200

    Hello Window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,57 @@
-#include <board.h>
+#include <SDL2/SDL.h>
+#include "board.h"
+
+const int WIDTH = 800, HEIGHT = 600;
+
+SDL_Window* create_game_window() {
+    // Oc
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    return SDL_CreateWindow(
+        "CuGO Game",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WIDTH,
+        HEIGHT,
+        SDL_WINDOW_ALLOW_HIGHDPI 
+    );
+}
+
+SDL_Renderer* create_game_renderer(SDL_Window *window) {
+    // Oc
+    SDL_Renderer *renderer;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT);
+
+    return renderer;
+}
 
 int main() {
-    board_t board;
-    
-    init_board(board);
-    render_board(board);
+    // Oc
+    SDL_Window *window = create_game_window();
+    SDL_Renderer *renderer = create_game_renderer(window); 
+    SDL_Event windowEvent;
+
+    bool exit_game_loop = false;
+
+    board_t *board = create_board(8);
+
+    do {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // render_board(board);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_WaitEvent(&windowEvent);
+        if (windowEvent.type == SDL_QUIT)
+            exit_game_loop = true;
+    } while (!exit_game_loop);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
