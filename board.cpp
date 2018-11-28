f43b31a936eb5f8c13daeca54a8de172f295131c
Zumza123 <coctavian10@yahoo.ro>
2018-11-28 19:10:44 +0200

    Code refactoring Changes for board.h and board.c:
    element_t -> piece_e
    board_t.board[][] -> board_t.piece[][]
    place_element_board -> place_piece_board
    move_element_board -> move_piece_board
    
    board_t is used as a pointer now
    
    + include-guard for board.h

--- board.cpp
+++ board.cpp
@@ -1,33 +1,38 @@
-#include <board.h>
+#include "board.h"
 
-void init_board(board &board, uint_t length) {
-    board.moves = 0;
-    board.length = length;
-    board.board = new *element_t[length];
+board_t* create_board(uint_t length) {
+    board_t *board = new board_t;
 
-    for (auto i = 0; i < board.length; i++)
-        board.board[i] = new element_t[length];
+    board->moves = 0;
+    board->length = length;
+    board->piece = new piece_e*[length];
+
+    for (uint_t i = 0; i < length; i++)
+        board->piece[i] = new piece_e[length];
+    
+    return board;
 }
 
-void place_element_board(board_t board, pos_t p, element_t e) {
-    board.board[p.x][p.y]=e;
-    board.moves++;
+void place_piece_board(board_t *board, pos_t p, piece_e e) {
+    // Ot
+    board->piece[p.x][p.y] = e;
+    board->moves++;
 }
 
-void move_element_board(board_t& board, pos_t p1, pos_t p2) {
-    place_element_board(board, p1, board.board[p2.x][p2.y]);
-    place_element_board(board, p2, element_t.EMPTY);
-    board.moves++;
+void move_piece_board(board_t *board, pos_t p1, pos_t p2) {
+    // Ot
+    place_piece_board(board, p1, board->piece[p2.x][p2.y]);
+    place_piece_board(board, p2, EMPTY);
+    board->moves++;
 }
 
-void draw_board_lines(board_t&);
+void draw_board_lines(board_t*);
 
-void draw_board_element(pos_t, element_t);
+void draw_board_piece(pos_t, piece_e);
 
-void draw_board_elements(board_t&);
+void draw_board_pieces(board_t*);
 
-
-void render_board(board_t& board) {
-    draw_board_lines();
-    draw_board_elements();
+void render_board(board_t *board) {
+    // draw_board_lines(board);
+    // draw_board_pieces(board);
 }
--- board.h
+++ board.h
@@ -1,26 +1,33 @@
+#ifndef CUGO_BOARD_H
+#define CUGO_BOARD_H
+
 #include <stdint.h>
 
+typedef unsigned int uint_t;
+
+enum piece_e {
+    EMPTY = 0,
+    PLAYER1 = 1,
+    PLAYER2 = 2
+};
+
+struct pos_t {
+    uint_t x;
+    uint_t y;
+};
+
 struct board_t {
-    element_t **board;
+    piece_e **piece;
     uint_t length;
     uint_t moves;
 };
 
-enum element_t {
-    EMPTY = 0,
-    PLAYER1 = 1,
-    PLAYER2 = 2
-}
+board_t* create_board(uint_t);
 
-struct pos_t {
-    uint_t x, y;
-};
+void place_piece_board(board_t*, pos_t, piece_e);
 
-void init_board(board_t&, uint_t);
-
-void place_element_board(board_t&, pos_t, element_t);
-
-void move_element_board(board_t&, pos_t, pos_t);
+void move_piece_board(board_t*, pos_t, pos_t);
 
 void render_board(board_t);
 
+#endif // CUGO_BOARD_H
