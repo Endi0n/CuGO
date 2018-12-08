ab3c2573fa4a3403137ec811052baf567b7c357a
Zumza123 <coctavian10@yahoo.ro>
2018-12-08 19:27:25 +0200

    Improvements for list.cpp and list.h

--- list.cpp
+++ list.cpp
@@ -1,36 +1,83 @@
 #include "list.h"
 
-void list_init(STRUCT_NAME(TYPE) *&list) {
-    list = new STRUCT_NAME(TYPE);
+void list_init(LIST_NAME(TYPE) *&list) {
+    list = new LIST_NAME(TYPE);
+    list->first = list->last = nullptr;
     list->length = 0;
 }
 
-bool list_is_empty(STRUCT_NAME(TYPE) *list) {
+inline bool list_is_empty(LIST_NAME(TYPE) *list) {
     return (list->length == 0);
 }
 
-void list_delete_first(STRUCT_NAME(TYPE) *list) {
-    if (list_is_empty(list)) return;
-    NODE_NAME(TYPE) *node = list->first->next;
-    delete list->first;
+void list_prepend(LIST_NAME(TYPE) *list, const TYPE &value) {
+    NODE_NAME(TYPE) *node = new NODE_NAME(TYPE);
+    node->value = value;
+    node->next = list->first;
+    node->prev = nullptr;
+
+    if (!list->first) list->first = node;
+    else list->first->prev = node;
+
     list->first = node;
-    list->length--;
+    list->length++;
 }
 
-void list_delete(STRUCT_NAME(TYPE) *list) {
-    while (!list_is_empty(list))
-        list_delete_first(list);
-    delete list;
-}
-
-void list_add(STRUCT_NAME(TYPE) *list, TYPE value) {
+void list_append(LIST_NAME(TYPE) *list, const TYPE &value) {
     NODE_NAME(TYPE) *node = new NODE_NAME(TYPE);
     node->value = value;
     node->next = nullptr;
+    node->prev = list->last;
 
-    if (list_is_empty(list)) list->first = node;
+    if (!list->first) list->first = node;
     else list->last->next = node;
 
     list->last = node;
     list->length++;
+}
+
+NODE_NAME(TYPE)* list_find_node(NODE_NAME(TYPE) *node, const TYPE &value) {
+    for (; node; node = node->next) {
+        uint_t n = sizeof(TYPE);
+        for (
+            unsigned char *a = (unsigned char*)&node->value, *b = (unsigned char*)&value;
+            n && *a == *b;
+            --n, ++a, ++b
+        );
+        if (!n) return node;
+    }
+    return nullptr;
+}
+
+TYPE* list_find(NODE_NAME(TYPE) *node, const TYPE &value) {
+    NODE_NAME(TYPE) *found_node = list_find_node(node, value);
+    if (!found_node) return nullptr;
+    return &found_node->value;
+}
+
+bool list_contains(LIST_NAME(TYPE) *list, const TYPE &value) {
+    return (list_find_node(list->first, value) != nullptr);
+}
+
+void list_delete(LIST_NAME(TYPE) *list, NODE_NAME(TYPE) *node) {
+    if (!node) return;
+    if (list->first == node)
+        list->first = node->next;
+    else {
+        node->prev->next = node->next;
+        if (node->next) node->next->prev = node->prev;
+        else list->last = node->prev;
+    }
+    delete node;
+    list->length--;
+}
+
+void list_delete(LIST_NAME(TYPE) *list, const TYPE &value) {
+    list_delete(list, list_find_node(list->first, value));
+}
+
+void list_delete(LIST_NAME(TYPE) *list) {
+    while (!list_is_empty(list))
+        list_delete(list, list->last);
+    delete list;
 }
--- list.h
+++ list.h
@@ -1,25 +1,36 @@
 typedef unsigned int uint_t;
 #define TOKEN_PASTE(x, y) x ## y
 #define NODE_NAME(T) TOKEN_PASTE(list_node_, T)
-#define STRUCT_NAME(T) TOKEN_PASTE(list_, T)
+#define LIST_NAME(T) TOKEN_PASTE(list_, T)
 
 struct NODE_NAME(TYPE) {
     TYPE value;
     NODE_NAME(TYPE) *next;
+    NODE_NAME(TYPE) *prev;
 };
 
-struct STRUCT_NAME(TYPE) {
+struct LIST_NAME(TYPE) {
     uint_t length;
     NODE_NAME(TYPE) *first;
     NODE_NAME(TYPE) *last;
 };
 
-void list_init(STRUCT_NAME(TYPE)*&);
+void list_init(LIST_NAME(TYPE)*&);
 
-bool list_is_empty(STRUCT_NAME(TYPE)*);
+inline bool list_is_empty(LIST_NAME(TYPE)*);
 
-void list_delete_first(STRUCT_NAME(TYPE)*);
+void list_prepend(LIST_NAME(TYPE)*, const TYPE&);
 
-void list_delete(STRUCT_NAME(TYPE)*);
+void list_append(LIST_NAME(TYPE)*, const TYPE&);
 
-void list_add(STRUCT_NAME(TYPE)*, TYPE);
+NODE_NAME(TYPE)* list_find_node(NODE_NAME(TYPE)*, const TYPE&);
+
+TYPE* list_find(NODE_NAME(TYPE)*, const TYPE&);
+
+bool list_contains(LIST_NAME(TYPE)*, const TYPE&);
+
+void list_delete(LIST_NAME(TYPE)*, NODE_NAME(TYPE)*);
+
+void list_delete(LIST_NAME(TYPE)*, const TYPE&);
+
+void list_delete(LIST_NAME(TYPE)*);
