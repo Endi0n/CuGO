70848f052a5c3fcbf4dc0bf610c637049ab46364
Zumza123 <coctavian10@yahoo.ro>
2018-12-17 10:51:26 +0200

    Logo Rendering Added render_logo and render_text functions

--- render.h
+++ render.h
@@ -9,7 +9,8 @@ SDL_Renderer* render_create_renderer(SDL_Window*);
 void SDL_SetRenderDrawColor(SDL_Renderer*, SDL_Color);
 
 void render_clear(SDL_Renderer*, SDL_Color);
-
+void render_text(SDL_Renderer*, char*, uint_t, SDL_Rect, SDL_Color);
+void render_logo(SDL_Renderer*);
 void render_board(SDL_Renderer*, board_t*, const SDL_Color[2]);
 
 #endif // CUGO_RENDER_H
