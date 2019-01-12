8ede42fab041403ecf2abfffef2ccadaef886f5e
Zumza123 <coctavian10@yahoo.ro>
2019-01-12 14:49:44 +0200

    Implemented Menu

--- render.h
+++ render.h
@@ -13,6 +13,7 @@ void render_present();
 void render_rect(const SDL_Rect&, SDL_Color);
 void render_circle(int, int, int, SDL_Color);
 void render_text(const char*, uint_t, point_t, SDL_Color);
+void render_button(const SDL_Rect&, const char*, SDL_Color, SDL_Color);
 void render_logo();
 void render_board_piece_selector(board_t*, point_t, const color_scheme_t&);
 void render_board(board_t*, const color_scheme_t&);
