87d1eae9f47d9c87e9712b6b0f8b535c92e06a5d
Zumza123 <coctavian10@yahoo.ro>
2019-01-12 14:30:40 +0200

    Renamed render_filled_circle to render_circle

--- render.cpp
+++ render.cpp
@@ -46,13 +46,13 @@ void render_deinit() {
     SDL_Quit();
 }
 
-void render_present() {
-    SDL_RenderPresent(renderer);
-}
-
 void render_clear(SDL_Color color) {
     SDL_SetRenderDrawColor(renderer, color);
     SDL_RenderClear(renderer);
+}
+
+void render_present() {
+    SDL_RenderPresent(renderer);
 }
 
 void render_rect(const SDL_Rect &rect, SDL_Color color) {
@@ -60,31 +60,9 @@ void render_rect(const SDL_Rect &rect, SDL_Color color) {
     SDL_RenderFillRect(renderer, &rect);
 }
 
-void render_text(const char *text, uint_t size, point_t pos, SDL_Color color) {
-    TTF_Font *font = TTF_OpenFont("sans.ttf", size);
-    SDL_Surface *surface = TTF_RenderText_Blended(font, text, color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+void render_circle(int cx, int cy, int radius, SDL_Color color) {
+    SDL_SetRenderDrawColor(renderer, color);
 
-    SDL_Rect rect = {.x = pos.x, .y = pos.y};
-    TTF_SizeText(font, text, &rect.w, &rect.h);
-
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-    TTF_CloseFont(font);
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(surface);
-}
-
-void render_logo() {
-    render_text(
-        "CuGO",
-        LOGO_SIZE,
-        {LOGO_OFFSET_X, LOGO_OFFSET_Y},
-        {0, 0, 0} // Render with black color 
-    );
-}
-
-void render_filled_circle(int cx, int cy, int radius) {
     int x = radius - 1;
     int y = 0;
     int dx = 1;
@@ -122,6 +100,30 @@ void render_filled_circle(int cx, int cy, int radius) {
     }
 }
 
+void render_text(const char *text, uint_t size, point_t pos, SDL_Color color) {
+    TTF_Font *font = TTF_OpenFont("sans.ttf", size);
+    SDL_Surface *surface = TTF_RenderText_Blended(font, text, color);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_Rect rect = {.x = pos.x, .y = pos.y};
+    TTF_SizeText(font, text, &rect.w, &rect.h);
+
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    TTF_CloseFont(font);
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+}
+
+void render_logo() {
+    render_text(
+        "CuGO",
+        LOGO_SIZE,
+        {LOGO_OFFSET_X, LOGO_OFFSET_Y},
+        {0, 0, 0} // Render with black color 
+    );
+}
+
 void render_board_cell(int x, int y, SDL_Color color) {
     SDL_Rect rect = {
         BOARD_OFFSET_X + x * BOARD_CELL_SIZE,
@@ -139,37 +141,38 @@ void render_board_grid(board_t *board, const color_scheme_t &color_scheme) {
             render_board_cell(x, y, color_scheme.board_cell_colors[(x + y) % 2]);
 }
 
-void render_board_piece(point_t piece) {
-    render_filled_circle(
+void render_board_piece(point_t piece, SDL_Color color) {
+    render_circle(
         BOARD_OFFSET_X + BOARD_CELL_SIZE * piece.x + BOARD_CELL_SIZE / 2,
         BOARD_OFFSET_Y + BOARD_CELL_SIZE * piece.y + BOARD_CELL_SIZE / 2,
-        BOARD_CELL_SIZE / 3
+        BOARD_CELL_SIZE / 3,
+        color
     );
 }
 
 void render_board_pieces(list_point_t *pieces, SDL_Color color) {
-    SDL_SetRenderDrawColor(renderer, color);
     for (list_node_point_t *node = pieces->first; node; node = node->next)
-        render_board_piece(node->value);
+        render_board_piece(node->value, color);
 }
 
-void render_board_potential_piece(point_t piece) {
-    render_filled_circle(
+void render_board_potential_piece(point_t piece, SDL_Color color) {
+    render_circle(
         BOARD_OFFSET_X + BOARD_CELL_SIZE * piece.x + BOARD_CELL_SIZE / 2,
         BOARD_OFFSET_Y + BOARD_CELL_SIZE * piece.y + BOARD_CELL_SIZE / 2,
-        BOARD_CELL_SIZE / 10
+        BOARD_CELL_SIZE / 10,
+        color
     );
 }
 
 void render_board_piece_selector(board_t *board, point_t piece, const color_scheme_t &color_scheme) {
-    SDL_SetRenderDrawColor(renderer, color_scheme.player_piece_colors[board_current_player(board)]);
+    SDL_Color color = color_scheme.player_piece_colors[board_current_player(board)];
 
     list_point_t *potential_moves;
     list_init(potential_moves);
     board_potential_moves(board, piece, potential_moves);
 
     for (list_node_point_t *node = potential_moves->first; node; node = node->next)
-        render_board_potential_piece(node->value);
+        render_board_potential_piece(node->value, color);
 
     list_delete(potential_moves);
 }
@@ -185,8 +188,7 @@ void render_turn_info(board_t *board, const color_scheme_t &color_scheme) {
     static const char *move = "has to move";
     const char *msg = (board->moves < board->size * 2) ? place : move;
     
-    SDL_SetRenderDrawColor(renderer, color_scheme.player_piece_colors[board_current_player(board)]);
-    render_filled_circle(30,  30, 12);
+    render_circle(30,  30, 12, color_scheme.player_piece_colors[board_current_player(board)]);
     render_text(
         msg,
         14,
