b8626fb89b3059f3a126dd88cbca8d43fda17108
Zumza123 <coctavian10@yahoo.ro>
2019-01-12 14:30:40 +0200

    Renamed render_filled_circle to render_circle

--- render.h
+++ render.h
@@ -9,8 +9,9 @@ void SDL_SetRenderDrawColor(SDL_Color);
 
 void render_init();
 void render_clear(SDL_Color);
-void render_rect(const SDL_Rect&, SDL_Color);
 void render_present();
+void render_rect(const SDL_Rect&, SDL_Color);
+void render_circle(int, int, int, SDL_Color);
 void render_text(const char*, uint_t, point_t, SDL_Color);
 void render_logo();
 void render_board_piece_selector(board_t*, point_t, const color_scheme_t&);
